a5541e432f76852bdc290daa627f615222c46aa1
MomtajHossainMow <momtajhossain2001@gmail.com>
2025-10-23 23:09:38 +0600

    first commit

---
+++ Bubble_Sort.py
@@ -0,0 +1,23 @@
+#Function to perform bubble sort on a given list
+def bubble_sort(arr):
+    l = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:   # Swap if the element found is greater
+                temp = arr[j]       # Swap operation
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+    return arr
+
+if __name__ == "__main__":
+    print("Enter the number of elements: ")
+    n = int(input())
+    arr = []
+    print("Enter the elements: ")
+    for i in range(n):
+        element = int(input())
+        arr.append(element)
+
+    sorted_arr = bubble_sort(arr)
+    print("Sorted array is: "+str(sorted_arr))
+    
